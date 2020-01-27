5bb5c8785fb33e26695dd5e84ec181d0b23ab8dd
ljazzal <jazzalin.main@gmail.com>
2020-01-27 12:58:59 -0500

    added custom srv and msg

--- smartsnipe_control/src/door_control.py
+++ smartsnipe_control/src/door_control.py
@@ -1,26 +1,58 @@
 import rospy
-from actuate_door import ActuateDoor
+from smartsnipe_msgs.srv import ActuateDoor, ActuateDoorRequest, ActuateDoorResponse
+from smartsnipe_msgs.msg import Shot
+from std_msgs import UInt8
 
-class DoorActuator:
+class Door:
+    def __init__(self, label):
+        self.id = label
+        self.open = False
+        self.count = 0
+        # TODO: add field to control speed of door
+        # self.speed = 127
 
-    def __init__(self):
-        self.d1
-
-    def actuate_door(self, door):
-        request = AcuateDoorRequest()
-        response = ActuateDoorResponse()
-
-        
-
-
+    def increment_count(self):
+        self.count += 1
 
 
+class DoorMonitor:
+
+    def __init__(self, doors=[1, 1, 1, 1, 1]):
+        """
+        Control interface to actuate and monitor doors independently
+            doors: mask list indicating which doors to control (default: all) e.g. [0, 1, 1, 0, 1]
+        """
+        self.doors = []
+        for index in doors:
+            if doors[index]:
+                self.doors.append(Door(index))
+
+        rospy.Subscriber('shot', Shot, self.shot_cb)
+
+
+    def actuate_door(self, door, open):
+        """Blocking call to open/close door(s)"""
+        rospy.wait_for_service('doors')
+        try:
+            actuator = rospy.ServiceProxy('doors', ActuateDoor)
+            req = ActuateDoorRequest(door=door, open=open)
+            resp = actuator(req)
+            rospy.loginfo(resp.message)
+        except rospy.ServiceException as e:
+            print("Call to ActuateDoor service failed: %s", e)
+
+
+    def shot_cb(self, data):
+        """Update internal state of targets for analytics"""
+        # TODO: record "shot" msg in json/dict format for db storage
+        self.doors[data.door].increment_count()
 
 
 if __name__ == "__main__":
     try:
         rospy.init_node('door_actuator')
-        client = DoorActuator()
+        # door_mask = [0, 0, 0, 1, 1] # e.g.
+        client = DoorMonitor()
         rospy.spin()
     except rospy.ROSInterruptException:
         print("Door actuation node interrupted before completion")
